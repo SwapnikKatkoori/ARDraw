aeb571d8fb1680ca7f144fbc9b8350b2c5e1bd15
Swapnik Katkoori <katkoor2@msu.edu>
2019-03-21 01:51:05 -0400

    initial commit

---
+++ ARDraw/ARDraw/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ARDraw
+//
+//  Created by Swapnik R. Katkoori on 3/21/19.
+//  Copyright © 2019 Swapnik R. Katkoori. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+}
