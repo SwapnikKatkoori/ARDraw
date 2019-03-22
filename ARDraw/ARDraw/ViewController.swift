3be7b87b0afe4b45a7e6ae3ed1e29f7d3b8af302
Swapnik Katkoori <katkoor2@msu.edu>
2019-03-21 23:17:47 -0400

    Draws with camera movement now

--- ARDraw/ARDraw/ViewController.swift
+++ ARDraw/ARDraw/ViewController.swift
@@ -32,15 +32,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         drawButton.addTarget(self, action: #selector(endDraw), for: .touchUpInside)
         drawButton.addTarget(self, action: #selector(startDraw), for: .touchDown)
         view.addSubview(drawButton)
+        sceneView.autoenablesDefaultLighting = true
+
         
     }
     
     @objc func endDraw(){
-        touched = false
+        DispatchQueue.main.async {
+            self.touched = false
+        }
+        
     }
     
     @objc func startDraw(){
-        touched = true
+        DispatchQueue.main.async {
+            self.touched = true
+        }
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -59,18 +66,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    func get_relative_position(_ current_frame: ARFrame, _ node:SCNNode, _ node_to_add: SCNNode){
+        let camera = current_frame.camera
+        let transform = camera.transform
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.1
+        node.simdTransform = matrix_multiply(transform, translation)
+        node_to_add.addChildNode(node)
+    }
+    
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         if touched == true{
-            let ball = SCNSphere(radius: 0.01)
+            let ball = SCNSphere(radius: 0.005)
             let node = SCNNode()
             let material = SCNMaterial()
             material.diffuse.contents = UIColor.red
             ball.materials = [material]
             node.geometry = ball
-            node.position = SCNVector3Make(x, 0, -0.5)
-            sceneView.autoenablesDefaultLighting = true
-            sceneView.scene.rootNode.addChildNode(node)
-            x += 0.01
+            if let frame = sceneView.session.currentFrame{
+                let node_to_add = sceneView.scene.rootNode
+                get_relative_position(frame, node, node_to_add)
+            }
+
+            
         }
     }
 
